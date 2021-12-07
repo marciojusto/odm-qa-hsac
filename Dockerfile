5e434bba3648e3fcb4a18a4fa1a26505f786682f
Marcio Oliveira <marcio.oliveira@teamwillgroup.com>
2021-12-07 15:03:31 +0000

    first commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM hsac/fitnesse-fixtures-test-jre8-chrome:latest
+EXPOSE 9090
+ARG JAR_FILE=target/*.jar
+COPY ${JAR_FILE} app.jar
+ENTRYPOINT [ "java", \
+             "-jar", \
+             "/app.jar"]
---
+++ buildChrome.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+VERSION_SUFFIX=$1
+SELENIUM_VERSION=${2:-4.1.0}${VERSION_SUFFIX}
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+IMAGE=hsac/fitnesse-fixtures-test-jre8-chrome:latest${VERSION_SUFFIX}
+
+docker pull selenium/standalone-chrome:${SELENIUM_VERSION}
+
+docker build --build-arg SELENIUM_VERSION=${SELENIUM_VERSION} -t ${IMAGE} chrome
+
+retVal=$?
+if [ ${retVal} -eq 0 -a "${TEST_IMAGES}" = "true" ]; then
+    docker run --rm \
+        -v ${BASEDIR}/target/failsafe-reports:/fitnesse/target/failsafe-reports \
+        -v ${BASEDIR}/target/fitnesse-results/chrome${VERSION_SUFFIX}:/fitnesse/target/fitnesse-results \
+        -v ${BASEDIR}/target/fitnesse-results/chrome-rerun${VERSION_SUFFIX}:/fitnesse/target/fitnesse-rerun-results \
+        -v ${BASEDIR}/target/selenium-log${VERSION_SUFFIX}:/fitnesse/target/selenium-log \
+        -v ${BASEDIR}/src/main/wiki:/fitnesse/wiki/FitNesseRoot \
+        -e RE_RUN_FAILED=true \
+        ${IMAGE} \
+        -DfitnesseSuiteToRun=AcceptanceTests.FrontEndTests.GoogleTest
+    retVal=$?
+fi
+exit ${retVal}
